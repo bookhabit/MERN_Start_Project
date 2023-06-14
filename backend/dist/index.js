511329f9b8ff818aa1626b6484b5646d6fb34182
bookhabit <qnrgoqlt@naver.com>
2023-06-14 12:16:32 +0900

    몽고DB연결

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -6,10 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const models_1 = __importDefault(require("./models"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({ credentials: true, origin: 'http://localhost:5173' }));
+// 몽고DB 연결
+(0, models_1.default)();
 app.get('/', (req, res) => {
     res.send('Hello World From the Typescript Server!');
 });
--- backend/index.ts
+++ backend/index.ts
@@ -3,12 +3,17 @@ import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 import path from "path";
 import cors from "cors";
+import mongoose from 'mongoose'
+import connectToMongoDB from "./models";
 
 dotenv.config();
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors({credentials:true,origin:'http://localhost:5173'}));
+
+// 몽고DB 연결
+connectToMongoDB();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World From the Typescript Server!')
