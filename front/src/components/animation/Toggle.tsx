94dc66f29152c12235bb0c56e5bd730434125cb3
bookhabit <qnrgoqlt@naver.com>
2023-06-29 19:46:29 +0900

    토글버튼의 다양한 속성들

--- front/src/components/animation/Toggle.tsx
+++ front/src/components/animation/Toggle.tsx
@@ -4,12 +4,15 @@ import { useState } from 'react';
 const Toggle = () => {
     const [isToggled,setToggle] = useState(false)
     const fade = useSpring({
-        opacity:isToggled?1:0
+        // opacity:isToggled?1:0,
+        // fontSize:isToggled?"2rem":"5rem",
+        color:isToggled?"tomato":"green",
+        transform:isToggled?"translate3d(0,0,0)":"translate3d(0,-50px,0)",
     })
     return (
-        <div>
-            <animated.h1 style={fade}>Hello</animated.h1>
-            <button onClick={()=>setToggle(!isToggled)}>Toggle</button>
+        <div className='flex flex-col items-center justify-center gap-5 min-h-screen'>
+            <animated.h1 style={fade} className={'text-2xl font-bold'}>Hello</animated.h1>
+            <button className='bg-black text-white p-5' onClick={()=>setToggle(!isToggled)}>Toggle</button>
         </div>
     );
 };
