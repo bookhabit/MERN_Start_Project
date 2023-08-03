c4f3efca6ea1262779467ed7cbc3c13e79e776c5
bookhabit <qnrgoqlt@naver.com>
2023-08-04 08:40:48 +0900

    socket.io로 프론트 백엔드 연결

--- front/src/pages/ChattingPage.tsx
+++ front/src/pages/ChattingPage.tsx
@@ -1,18 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Input } from '../elements';
 import { InputChangeEvent } from '../elements/Input';
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:4000"); // 백엔드 서버의 주소
 
 const ChattingPage = () => {
-    const [value,setValue] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [messageInput, setMessageInput] = useState("");  
     const [errMsg,setErrMsg] = useState('');
 
     const onChange = (event:InputChangeEvent)=>{
-        setValue(event.target.value)
+        setMessageInput(event.target.value)
     }
 
-    const sendHandler = ()=>{
-
+    const handleSendMessage  = ()=>{
+        socket.emit("chat message", messageInput);
+        setMessageInput("");
     }
+
+    // 채팅연결
+    useEffect(() => {
+        socket.on("chat message", (message) => {
+            console.log(message)
+        });
+
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
 
     return (
         <div className='flex h-screen'>
@@ -22,20 +38,23 @@ const ChattingPage = () => {
             <div className='bg-blue-100 w-2/3 flex flex-col p-2'>
                 <div className='flex-grow'>
                     messages with selected person
+                    {messages.map((message, index) => (
+                        <div key={index}>{message}</div>
+                    ))}
                 </div>
                 <div className='flex items-center gap-2 mb-5'>
                     <Input 
                         placeholder='메시지를 입력해주세요'    
                         sort="chatting"
                         type='text'
-                        value={value}
+                        value={messageInput}
                         name="chatting"
                         _onChange={onChange}
                         isValid={true}
                         errorMessage={errMsg}
                         validateMode={false}
                     />
-                    <Button _onClick={sendHandler} sort="chatting" />
+                    <Button _onClick={handleSendMessage } sort="chatting" />
                 </div>
             </div>
         </div>
